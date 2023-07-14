f733af288b85503f3a14b8f09b40ee74e12adbcf
Laurel-Amah <laurelngumamah@gmail.com>
2023-07-13 19:36:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/*This program prints a text using puts*/
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-puts(Puts[] = "Programming is like building a multilingual puzzle \n";
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
