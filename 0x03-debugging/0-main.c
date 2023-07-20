a218212dc6c1ac8498a6f10fd151a16aed694e25
Laurel-Amah <laurelngumamah@gmail.com>
2023-07-20 14:20:44 -0700

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,6 +4,7 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
+
 int main(void)
 {
 	int i;
@@ -13,4 +14,3 @@ int main(void)
 
 	return (0);
 }
-
--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -5,19 +5,15 @@
 * main - prints the largest of 3 integers
 * Return: 0
 */
-
 int main(void)
 {
-        int a, b, c;
-        int largest;
+	int a, b, c;
+	int largest;	
+	a = 972
+	b = -98;
+	c = 0;
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
 
-        a = 972;
-        b = -98;
-        c = 0;
-
-        largest = largest_number(a, b, c);
-
-        printf("%d is the largest number\n", largest);
-
-        return (0);
+	return (0);
 }
