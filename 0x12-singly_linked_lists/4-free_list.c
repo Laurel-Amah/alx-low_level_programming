14d9946ead64c8b0e7746fe16214ca1b15d1b7f0
Laurel Amah <laurelngumamah@gmail.com>
2023-10-20 10:55:57 -0700

    singly linked lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,15 +5,14 @@
  * @head: head of the linked list.
  * Return: no return.
  */
-
 void free_list(list_t *head)
 {
-    list_t *current;
+	list_t *current;
 
-    while ((current = head) != NULL)
-    {
-        head = head->next;
-        free(current->str);
-        free(current);
-    }
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
