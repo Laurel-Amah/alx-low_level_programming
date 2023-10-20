d852072197d90f3655bcf6d395515ccb42c69dec
Laurel Amah <laurelngumamah@gmail.com>
2023-10-20 15:51:31 -0700

    singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void bfmain(void) __attribute__((constructor));
+
+/**
+ * bfmain - executes before main function
+ *
+ */
+void bfmain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
