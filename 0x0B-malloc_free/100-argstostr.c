e770c06e7a3727031becf0803e058dccd9a4c855
Laurel Amah <laurelngumamah@gmail.com>
2023-10-31 19:29:52 -0700

    malloc free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * argstostr- concates all arguments
+ * @ac: the number of arguments
+ * @av: the array containing argument vectors
+ * Return: pav
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+	int k = 0;
+	int len = 0;
+	char *pav;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+		len++;
+	}
+	pav = malloc((len + 1) * (sizeof(char)));
+	if (pav == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			pav[k] = av[i][j];
+			k++;
+		}
+		pav[k] = '\n';
+		k++;
+	}
+	pav[len] = '\0';
+	return (pav);
+	free(pav);
+}
